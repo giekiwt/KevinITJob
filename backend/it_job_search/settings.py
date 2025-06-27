f94a44fed490d259803a450e9bdbb6565b46fb4a
giakietit <giakiethcb@gmail.com>
2025-06-27 23:10:51 +0700

    Remake  css for job details

--- backend/it_job_search/settings.py
+++ backend/it_job_search/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'backend', 'it_job_search', 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
